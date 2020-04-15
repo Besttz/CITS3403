a60afbeca049d167adca26da28a3b118c519338c
Tommy <tommyzhangchina@gmail.com>
2020-04-16 03:30:28 +0800

    0.1.3 Homepage Basic
     完成了主页的第一板块基础构造 下一步使用 jQuery 实现更多功能

---
+++ Project/scripts/home.js
@@ -0,0 +1,7 @@
+defaultHead = "WELCOME TO WEB MUSEUM!";
+html = "HyperText Markup Language";
+css ="Cascading Style Sheets";
+js ="JavaScript";
+function changeHead(txt) {
+    document.getElementById("headText").innerHTML = txt;
+}
