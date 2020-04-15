40f55569f17bda9027c9dd2063af5ee342d79355
Tommy <tommyzhangchina@gmail.com>
2020-04-16 03:51:22 +0800

    0.1.4 Using JQuery

--- Project/scripts/home.js
+++ Project/scripts/home.js
@@ -1,7 +1,52 @@
 defaultHead = "WELCOME TO WEB MUSEUM!";
 html = "HyperText Markup Language";
-css ="Cascading Style Sheets";
-js ="JavaScript";
+css = "Cascading Style Sheets";
+js = "JavaScript";
 function changeHead(txt) {
     document.getElementById("headText").innerHTML = txt;
 }
+
+function iconOnHover(obj) {
+    $("#headText").fadeOut(500);
+    setTimeout(function () { changeHead(obj); }, 499);
+    $("#headText").fadeIn(500);
+}
+
+
+function iconOffHover() {
+    $("#headText").fadeOut(500);
+    setTimeout(function () { changeHead(defaultHead); }, 499);
+    $("#headText").fadeIn(500);
+}
+
+$(document).ready(function () {
+    $("#htmlIcon").hover(function () {
+        iconOnHover(html);
+    },
+        function () {
+            iconOffHover();
+        }
+    );
+    $("#cssIcon").hover(function () {
+        $("#headText").fadeOut(500);
+        setTimeout(function () { changeHead(css); }, 499);
+        $("#headText").fadeIn(500);
+    },
+        function () {
+            $("#headText").fadeOut();
+            setTimeout(function () { changeHead(defaultHead); }, 499);
+            $("#headText").fadeIn();
+        }
+    );
+    $("#jsIcon").hover(function () {
+        $("#headText").fadeOut(500);
+        setTimeout(function () { changeHead(js); }, 499);
+        $("#headText").fadeIn(500);
+    },
+        function () {
+            $("#headText").fadeOut();
+            setTimeout(function () { changeHead(defaultHead); }, 499);
+            $("#headText").fadeIn();
+        }
+    );
+});
