b7b43fa34171e27a02caee5a5ce4fcb546c191ca
Tommy <tommyzhangchina@gmail.com>
2020-04-17 00:26:32 +0800

    0.2.1.3 H  S1 Animation

--- Project/scripts/content.js
+++ Project/scripts/content.js
@@ -1,5 +1,17 @@
+function iconOnHover(obj) {
+}
+
+
 $(document).ready(function () {
     setTimeout(function () { $(".home-icons").fadeIn(300); }, 300);
     setTimeout(function () { $("#homeText").fadeIn(300); }, 400);
     setTimeout(function () { $("#homeP").fadeIn(300); }, 500);
+
+    $("#htmlIcon").hover(function () {
+        iconOnHover();
+    },
+        function () {
+            iconOffHover();
+        }
+    );
 });
