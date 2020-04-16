e2dc22bd84530afb30a7f63a0cd763d24e2a1d95
Tommy <tommyzhangchina@gmail.com>
2020-04-17 00:21:36 +0800

    0.2.1.2 H
    SEC 1 upgrade

--- Project/scripts/content.js
+++ Project/scripts/content.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    setTimeout(function () { $("#headText").fadeIn(300); }, 300);
-    setTimeout(function () { $(".home-icons").fadeIn(300); }, 400);
-    setTimeout(function () { $("#homeText").fadeIn(300); }, 500);
+    setTimeout(function () { $(".home-icons").fadeIn(300); }, 300);
+    setTimeout(function () { $("#homeText").fadeIn(300); }, 400);
+    setTimeout(function () { $("#homeP").fadeIn(300); }, 500);
 });
