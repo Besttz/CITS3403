a39e178fc70f80aefa83aea1200f3854c710e7f1
Tommy <tommyzhangchina@gmail.com>
2020-04-20 16:45:45 +0800

    3.3.1 DOM

--- Project/scripts/js.js
+++ Project/scripts/js.js
@@ -1,6 +1,51 @@
 max = 5; //FOR KEYBOARD PAGE CONTROL
-alerttxt='I am an alert box by JavaScript! To make your own one? \n'+
-'Simply add onclick="alert(content);" to any element tag.';
+alerttxt = 'I am an alert box by JavaScript! To make your own one? \n' +
+    'Simply add onclick="alert(content);" to any element tag.';
+
+function JSSelector(pageID) {
+
+    $("#j31").fadeOut(200);
+    $("#j32").fadeOut(200);
+
+    setTimeout(function () { $("#j" + pageID).fadeIn(200); }, 200);
+}
+
+function jsOnclick(jsid) {
+    for (let index = 0; index < 6; index++) {
+        $("#j34" + index).css("background-color", "rgba(145, 192, 236, 0.3)");
+    }
+    switch (jsid) {
+        case 0:
+            // for (let index = 30; index < 36; index++) {
+            //     $("#c" + index).css("background-color", "rgba(243, 200, 200, 0.8)");
+            // }
+            $("#j344").css("background-color", "rgba(145, 192, 255, 0.8)");
+
+            break;
+        case 1:
+            $("#j341").css("background-color", "rgba(145, 192, 255, 0.8)");
+            break;
+        case 2:
+            $("#j343").css("background-color", "rgba(145, 192, 255, 0.8)");
+            $("#j345").css("background-color", "rgba(145, 192, 255, 0.8)");
+            break;
+        case 3:
+            $("#j342").css("background-color", "rgba(145, 192, 255, 0.8)");
+            $("#j3401").fadeIn(300);
+            break;
+        case 4:
+            $("#j344").css("background-color", "rgba(145, 192, 255, 0.8)");
+            $("#j3402").html("../img.png");
+            break;
+        case 5:
+            $("#j342").css("background-color", "rgba(145, 192, 255, 0.8)");
+            $("#j3403").fadeIn(300);
+            break;
+
+        default:
+            break;
+    }
+}
 
 
 $(document).ready(function () {
