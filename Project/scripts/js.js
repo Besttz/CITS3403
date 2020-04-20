6e2a4f46545bce2c96d6ff9ca13a1b5c6ff9ed7a
Tommy <tommyzhangchina@gmail.com>
2020-04-20 17:55:38 +0800

    3.4

--- Project/scripts/js.js
+++ Project/scripts/js.js
@@ -2,6 +2,9 @@ max = 5; //FOR KEYBOARD PAGE CONTROL
 alerttxt = 'I am an alert box by JavaScript! To make your own one? \n' +
     'Simply add onclick="alert(content);" to any element tag.';
 
+exampleStart = false;
+
+
 function JSSelector(pageID) {
 
     $("#j31").fadeOut(200);
@@ -47,6 +50,63 @@ function jsOnclick(jsid) {
     }
 }
 
+
+function JSDemo(jID) {
+    if (!exampleStart) {
+        exampleStart = true;
+        $("#HExampleArea").addClass("col-lg-6");
+        $("#HECode").css("display", "");
+        $("#HExampleWelcome").fadeOut(300);
+        $("#HExampleDisplay").css("background-color", "#222");
+
+        setTimeout(function () {
+            $("#HExampleContent").fadeIn(300);
+        }, 300);
+        $("#j40").fadeIn(200);
+    }
+
+    if (jID = 0) {
+        $.ajax({
+            type: "GET",
+            url: 'https://api.openweathermap.org/data/2.5/weather?q=perth&appid=fb9eb902711a0edee2441da1d667dd01',
+            success: function (data) {
+                console.log(data);
+                // var x;
+                // var text;
+                // for (x in data) {
+                //     text += data[x] + "\n";
+                // }
+                $("#j40").text("Successful Get");
+                setTimeout(function () {
+                    $("#j410").text(data.name);
+                    $("#j411").text(data.weather[0].main);
+                    $("#j412").text(data.weather[0].description);
+                    $("#j414").text(data.wind.deg);
+                    $("#j415").text(data.wind.speed);
+                }, 1000);
+
+            },
+        });
+    } else if (jID = 1) {
+        $.ajax({
+            type: "GET",
+            url: 'https://coronavirus-tracker-api.herokuapp.com/v2/locations',
+            success: function (data) {
+                console.log(data);
+                $("#j40").text("Successful Get");
+                setTimeout(function () {
+                    $("#j410").text('Confirmed: ' + data.latest.confirmed);
+                    $("#j411").text('Deaths: ' + data.latest.deaths);
+                    $("#j412").text(" ");
+                    $("#j414").text(" ");
+                    $("#j415").text(" ");
+                }, 1000);
+
+            },
+        });
+    }
+
+}
 
 $(document).ready(function () {
     $("#j21").click(function () {
