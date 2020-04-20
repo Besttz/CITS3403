b2c5bfbc4019b779bdf0f688059c371b8860fead
Tommy <tommyzhangchina@gmail.com>
2020-04-20 15:47:58 +0800

    3.2.10 Done

--- Project/scripts/js.js
+++ Project/scripts/js.js
@@ -1,12 +1,18 @@
 max = 5; //FOR KEYBOARD PAGE CONTROL
+alerttxt='I am an alert box by JavaScript! To make your own one? \n'+
+'Simply add onclick="alert(content);" to any element tag.';
+
 
 $(document).ready(function () {
     $("#j21").click(function () {
+        $("#j232").fadeOut(300);
+        $("#j222").fadeOut(300);
         $("#j211").fadeIn(300);
         $("#j221").text("document.getElementById('heading').innerHTML='You Clicked'");
     }
     );
     $("#j22").click(function () {
+        $("#j232").fadeOut(300);
         $("#j211").fadeIn(300);
         $("#j221").text('"button()"');
         $("#j222").fadeIn(300);
